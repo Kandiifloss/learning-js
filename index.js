1b3fd78c6e9be572f12fbc84f527a5d1cf2a9add
Kandiifloss <1.2.990@mail.ru>
2026-03-08 04:42:50 +0300

    Таймер

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const timeDisplay = document.getElementById("time");
+const start = document.getElementById("startBtn");
+const stop = document.getElementById("stopBtn");
+const reset = document.getElementById("resetBtn");
+const displayTimer = document.getElementById("display");
+
+let isRunning = false;
+let clockMiliseconds = 0; 
+let startTime = 0;
+let running;
+
+function startTimer(){
+    if (!isRunning){
+        startTime = Date.now() - clockMiliseconds;
+        running = setInterval(update, 10);  
+        isRunning = true;
+        }
+    else return;
+    }
+
+function stopTimer(){
+    clearTimeout(running);
+    isRunning = false;
+    
+}
+
+function resetTimer(){
+    clearTimeout(running);
+    displayTimer.textContent = "00:00:00:00";
+    clockMiliseconds = 0;
+    isRunning = false;
+}
 
 function update(){
-    let = currentTime = new Date();
-    let ampm = currentTime.getHours() / 12 > 1 ? "PM": "AM" ;
-    timeDisplay.textContent = `${currentTime.getHours().toString().padStart(2, 0)%12}:`+
-                              `${currentTime.getMinutes().toString().padStart(2, 0)}:`+
-                              `${currentTime.getSeconds().toString().padStart(2, 0)}  `+
-                              `${ampm}`;
+    const now = Date.now()
+    clockMiliseconds = (now - startTime);
+    
+    let millisecons = Math.round(clockMiliseconds % 1000/10).toString().padStart(2,"0");
+    let seconds = Math.round(clockMiliseconds / (1000) % 60).toString().padStart(2,"0");
+    let minutes = Math.round(clockMiliseconds/(1000*60)%60).toString().padStart(2,"0");
+    let hours = Math.round(clockMiliseconds/(1000*60*60)).toString().padStart(2, "0");
+
+    displayTimer.textContent = `${hours}:${minutes}:${seconds}:${millisecons}`;
 }
-update();
-setInterval(update, 1000);
+
+start.onclick = startTimer;
+stop.onclick = stopTimer;
+reset.onclick = resetTimer;
